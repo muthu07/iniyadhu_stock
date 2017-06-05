f37b258973d54996aca574c564210508ee935776
mkamala <mkamala8@ford.com>
2017-06-05 20:08:04 +0530

    updated sql

--- stock.sql
+++ stock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2017 at 02:30 PM
+-- Generation Time: Jun 05, 2017 at 02:36 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
